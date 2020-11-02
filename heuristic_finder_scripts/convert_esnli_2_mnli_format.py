e4314e4d73a8be3dc7dd545d8fd122653ffcbf4f
rosafish <zhour@carleton.edu>
2020-11-01 21:17:51 -0800

    use esnli data for entries

--- heuristic_finder_scripts/convert_esnli_2_mnli_format.py
+++ heuristic_finder_scripts/convert_esnli_2_mnli_format.py
@@ -3,6 +3,13 @@
 # Note that SNLI is in (basically) the same format as MNLI, so just add explanation from esnli to SNLI and match the pairIDs
 import csv
 
+def list_to_line(alist):
+    result = ""
+    for item in alist:
+        result = result + item + '\t'
+    result = result[:-1] 
+    result = result + "\n"
+    return result
 
 def main():
     esnli_input_path = '/data/rosa/data/esnli/esnli_train.csv'
@@ -16,12 +23,14 @@ def main():
             if i == 0:
                 continue
             pairID = line[0]
-            # gold_label = line[1]
+            gold_label = line[1]
             gold_expl1 = line[4]
+            premise = line[2]
+            hypothesis = line[3]
             if pairID in id_expl_dict:
                 print('duplicate ID')
                 return
-            id_expl_dict[pairID] = gold_expl1
+            id_expl_dict[pairID] = (premise, hypothesis, gold_label, gold_expl1)
 
     fo = open(output_path, "w")
     # just keep the same things for position 0, 1, 5, 6
@@ -29,14 +38,25 @@ def main():
     snli_text_file = open(snli_input_path, "r") 
     snli_lines = snli_text_file.readlines()
     for (i, line) in enumerate(snli_lines):
+        parts = line.strip().split("\t")
+        parts = parts[:9]
         if i == 0:
-            fo.write(line[:-1] + "\tgold_expl1\n")
+            parts.append('gold_expl1')
+            fo.write(list_to_line(parts))
             continue
-        labels = line.strip().split("\t")
-        pairID = labels[8] 
+
+        pairID = parts[8] 
+        snli_label = parts[0]
         if pairID in id_expl_dict:
-            gold_expl1 = id_expl_dict[pairID]
-            fo.write(line[:-1] + "\t" + gold_expl1+ "\n")
+            info_tuple = id_expl_dict[pairID]
+            esnli_label = info_tuple[2]
+            if snli_label.lower() != esnli_label.lower():
+                print('label does not match between snli and esnli')
+            #make create the new line to write
+            parts[5] = info_tuple[0] # premise
+            parts[6] = info_tuple[1] # hypothesis
+            parts.append(info_tuple[3]) # expl1
+            fo.write(list_to_line(parts))
 
     fo.close()
     
--- heuristic_finder_scripts/subseq_finder.py
+++ heuristic_finder_scripts/subseq_finder.py
@@ -1,6 +1,12 @@
+import sys
+sys.path.append('/data/rosa/my_github/misinformation/code/')
+from myTools import write_csv
 
-f = open("esnli_train_subseq.txt", "a")
 fi = open("esnli_train.txt", "r")
+
+fo_path = './esnli_train_subseq.csv'
+fo_header = ['guid', 'label', 'premise', 'hypothesis', 'expl1', 'template']
+fo_row_data = []
 
 count_entailment = 0
 count_neutral = 0
@@ -12,6 +18,8 @@ for line in fi:
     premise = parts[5]
     hypothesis = parts[6]
     label = parts[0]
+    guid = parts[8]
+    expl1 = parts[-1]
 
     prem_words = []
     hyp_words = []
@@ -28,19 +36,19 @@ for line in fi:
     hyp_filtered = " ".join(hyp_words)
 
     if hyp_filtered in prem_filtered:
-        #print(premise, hypothesis, label, parts[1])
-        #print(label)
         if label == "entailment":
             count_entailment += 1
         if label == "neutral":
             count_neutral += 1
-            print(premise, hypothesis, label, file=f)
+            #print(premise, hypothesis, label)
+            fo_row_data.append([guid, label, premise, hypothesis, expl1, ""])
         if label == "contradiction":
             count_contradiction += 1
-            print(premise, hypothesis, label, file=f)
+            # print(premise, hypothesis, label)
+            fo_row_data.append([guid, label, premise, hypothesis, expl1, ""])
 
-    #print(premise, hypothesis, label)
+print("Entailment:", count_entailment)
+print("Contradiction:", count_contradiction)
+print("Neutral:", count_neutral)
 
-print("Entailment:", count_entailment, file=f)
-print("Contradiction:", count_contradiction, file=f)
-print("Neutral:", count_neutral, file=f)
+write_csv(fo_path, fo_row_data, fo_header)
