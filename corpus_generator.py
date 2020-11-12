60ace2d7022c5498b12fa1be150579608d721fcc
rosafish <zhour@carleton.edu>
2020-11-11 23:07:55 -0800

    generate dev and train data 30 templates

--- corpus_generator.py
+++ corpus_generator.py
@@ -1,40 +1,38 @@
 
 from templates import *
 
-# template_list = [
-#         ("lexical_overlap", "ln_subject/object_swap", "non-entailment", lex_simple_templates), 
-#         ("lexical_overlap", "ln_preposition", "non-entailment", lex_prep_templates), 
-#         ("lexical_overlap", "ln_relative_clause", "non-entailment", lex_rc_templates), 
-#         ("lexical_overlap", "ln_passive", "non-entailment", lex_pass_templates), 
-#         ("lexical_overlap", "ln_conjunction", "non-entailment", lex_conj_templates), 
-#         ("lexical_overlap", "le_relative_clause", "entailment", lex_rc_ent_templates), 
-#         ("lexical_overlap", "le_around_prepositional_phrase", "entailment", lex_cross_pp_ent_templates), 
-#         ("lexical_overlap", "le_around_relative_clause", "entailment",lex_cross_rc_ent_templates), 
-#         ("lexical_overlap", "le_conjunction", "entailment",lex_ent_conj_templates), 
-#         ("lexical_overlap", "le_passive", "entailment",lex_ent_pass_templates), 
-#         ("subsequence", "sn_NP/S", "non-entailment",subseq_nps_templates), 
-#         ("subsequence", "sn_PP_on_subject", "non-entailment",subseq_pp_on_subj_templates), 
-#         ("subsequence", "sn_relative_clause_on_subject", "non-entailment",subseq_rel_on_subj_templates), 
-#         ("subsequence", "sn_past_participle", "non-entailment",subseq_past_participle_templates), 
-#         ("subsequence", "sn_NP/Z", "non-entailment",subseq_npz_templates), 
-#         ("subsequence", "se_conjunction", "entailment",subseq_conj_templates), 
-#         ("subsequence", "se_adjective", "entailment", subseq_adj_templates), 
-#         ("subsequence", "se_understood_object", "entailment",subseq_understood_templates), 
-#         ("subsequence", "se_relative_clause_on_obj", "entailment",subseq_rel_on_obj_templates), 
-#         ("subsequence", "se_PP_on_obj", "entailment",subseq_pp_on_obj_templates), 
-#         ("constituent", "cn_embedded_under_if", "non-entailment",const_under_if_templates), 
-#         ("constituent", "cn_after_if_clause", "non-entailment",const_outside_if_templates),
-#         ("constituent", "cn_embedded_under_verb", "non-entailment",const_quot_templates), 
-#         ("constituent", "cn_disjunction", "non-entailment",const_disj_templates), 
-#         ("constituent", "cn_adverb", "non-entailment",const_advs_nonent_templates), 
-#         ("constituent", "ce_embedded_under_since", "entailment",const_adv_embed_templates), 
-#         ("constituent", "ce_after_since_clause", "entailment",const_adv_outside_templates),
-#         ("constituent", "ce_embedded_under_verb", "entailment",const_quot_ent_templates), 
-#         ("constituent", "ce_conjunction", "entailment",const_conj_templates), 
-#         ("constituent", "ce_adverb", "entailment",const_advs_ent_templates)
-#         ]
-
-template_list = [("lexical_overlap", "ln_preposition", "non-entailment", lex_prep_templates)]
+template_list = [
+        ("lexical_overlap", "ln_subject/object_swap", "non-entailment", lex_simple_templates), 
+        ("lexical_overlap", "ln_preposition", "non-entailment", lex_prep_templates), 
+        ("lexical_overlap", "ln_relative_clause", "non-entailment", lex_rc_templates), 
+        ("lexical_overlap", "ln_passive", "non-entailment", lex_pass_templates), 
+        ("lexical_overlap", "ln_conjunction", "non-entailment", lex_conj_templates), 
+        ("lexical_overlap", "le_relative_clause", "entailment", lex_rc_ent_templates), 
+        ("lexical_overlap", "le_around_prepositional_phrase", "entailment", lex_cross_pp_ent_templates), 
+        ("lexical_overlap", "le_around_relative_clause", "entailment",lex_cross_rc_ent_templates), 
+        ("lexical_overlap", "le_conjunction", "entailment",lex_ent_conj_templates), 
+        ("lexical_overlap", "le_passive", "entailment",lex_ent_pass_templates), 
+        ("subsequence", "sn_NP/S", "non-entailment",subseq_nps_templates), 
+        ("subsequence", "sn_PP_on_subject", "non-entailment",subseq_pp_on_subj_templates), 
+        ("subsequence", "sn_relative_clause_on_subject", "non-entailment",subseq_rel_on_subj_templates), 
+        ("subsequence", "sn_past_participle", "non-entailment",subseq_past_participle_templates), 
+        ("subsequence", "sn_NP/Z", "non-entailment",subseq_npz_templates), 
+        ("subsequence", "se_conjunction", "entailment",subseq_conj_templates), 
+        ("subsequence", "se_adjective", "entailment", subseq_adj_templates), 
+        ("subsequence", "se_understood_object", "entailment",subseq_understood_templates), 
+        ("subsequence", "se_relative_clause_on_obj", "entailment",subseq_rel_on_obj_templates), 
+        ("subsequence", "se_PP_on_obj", "entailment",subseq_pp_on_obj_templates), 
+        ("constituent", "cn_embedded_under_if", "non-entailment",const_under_if_templates), 
+        ("constituent", "cn_after_if_clause", "non-entailment",const_outside_if_templates),
+        ("constituent", "cn_embedded_under_verb", "non-entailment",const_quot_templates), 
+        ("constituent", "cn_disjunction", "non-entailment",const_disj_templates), 
+        ("constituent", "cn_adverb", "non-entailment",const_advs_nonent_templates), 
+        ("constituent", "ce_embedded_under_since", "entailment",const_adv_embed_templates), 
+        ("constituent", "ce_after_since_clause", "entailment",const_adv_outside_templates),
+        ("constituent", "ce_embedded_under_verb", "entailment",const_quot_ent_templates), 
+        ("constituent", "ce_conjunction", "entailment",const_conj_templates), 
+        ("constituent", "ce_adverb", "entailment",const_advs_ent_templates)
+        ]
 
 def no_the(sentence):
     return sentence.replace("the ", "")
@@ -80,7 +78,7 @@ def repeaters(sentence):
     else:
         return True
 
-fo = open("rosa_heuristics_test_set_2500.txt", "w")
+fo = open("rosa_heuristics_train_set_30T_15000.txt", "w")
 #fo.write("heuristic\tsubcase\ttemplate\tlabel\tpremise\thypothesis\tpremise_parse\thypothesis_parse\tpremise_binary_parse\thypothesis_binary_parse\n")
 fo.write("gold_label\tsentence1_binary_parse\tsentence2_binary_parse\tsentence1_parse\tsentence2_parse\tsentence1\tsentence2\tpairID\theuristic\tsubcase\ttemplate\tlow_q_expl\thigh_q_expl\n")
 
@@ -95,7 +93,7 @@ for template_tuple in template_list:
     example_dict = {}
     count_examples = 0
 
-    while count_examples < 2500:
+    while count_examples < 500:
         example = template_filler(template)
 
         example_sents = tuple(example[:2])
